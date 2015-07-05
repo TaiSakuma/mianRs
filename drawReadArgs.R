33e7041ba7b8949f4fcd0c446ebc07733b9e78e9
Tai Sakuma <tai.sakuma@gmail.com>
2015-07-05 19:51:25 +0200

    clean up code in drawReadArgs.R, add comments

--- drawReadArgs.R
+++ drawReadArgs.R
@@ -1,10 +1,28 @@
-# Tai Sakuma <sakuma@fnal.gov>
+# Tai Sakuma <sakuma@cern.ch>
 
-##____________________________________________________________________________||
+##__________________________________________________________________||
+## How to use this script.
+##
+## This script is designed to be used in a script that produces
+## lattice plots
+##
+## 1: parse this script
+## readArgs <- parse('drawReadArgs.R')
+##
+## 2: chose a theme (optional)
+## theme.default <- 'theme.economist'
+##
+## 3: evaluate readArgs
+## eval(readArgs)
+##
+
+##__________________________________________________________________||
+## set default theme
 theme.default <- if(exists('theme.default')) theme.default else 'theme.xsec'
 
-##____________________________________________________________________________||
-opts <- list(
+##__________________________________________________________________||
+## set default args
+opts_default <- list(
              list(option = 'arg.outdir', default = 'fig'),
              list(option = 'arg.pdf', default = TRUE),
              list(option = 'arg.png', default = TRUE),
@@ -14,38 +32,60 @@ opts <- list(
              list(option = 'arg.transparent', default = FALSE)
              )
 
-##____________________________________________________________________________||
-com.args <- commandArgs(trailingOnly = TRUE)
-cat('Args:', paste(com.args, sep = '', collapse = " "), '\n')
-for(arg in com.args) eval(parse(text = paste('arg.', com.args, sep = '')))
 
-##____________________________________________________________________________||
-for(opt in opts)
+options = sapply(opts_default, function(x) x[['option']])
+## i.e., c("arg.outdir", "arg.pdf", "arg.png", "arg.theme", "arg.id", "arg.title", "arg.transparent")
+
+
+##__________________________________________________________________||
+## read command line args
+com.args <- commandArgs(trailingOnly = TRUE)
+
+## print them on the screen
+cat('Args:', paste(com.args, sep = '', collapse = " "), '\n')
+
+##__________________________________________________________________||
+## assign options to variables e.g., "arg.outdir=\"fig_01\""
+for(arg in com.args)
+  {
+    ## e.g., arg = "outdir=\"fig_01\""
+
+    option <- strsplit(arg, split = "=")[[1]][1] # e.g., "outdir"
+    option <- paste('arg.', option, sep = '') # e.g., "arg.outdir"
+
+    if(!option %in% options) options <- c(options, option)
+
+    ## parse and evaluate a string like "arg.outdir=\"fig_01\""
+    ## which will actually assigns the value to the variable
+    eval(parse(text = paste('arg.', arg, sep = '')))
+  }
+
+##__________________________________________________________________||
+## assign default values if not assigned above
+for(opt in opts_default)
   {
     cmd <- paste(opt$option, ' <- if(exists("', opt$option, '")) ', opt$option, ' else opt$default', sep = '')
     eval(parse(text = cmd))
   }
 
-##____________________________________________________________________________||
+##__________________________________________________________________||
 show.options <- function()
   {
     cat('\n')
     cat('Options:\n')
-    for(opt in opts)
+    for(opt in options)
       {
-        opt.value = eval(parse(text = opt$option))
-        opt.value
-        cat(sprintf('%4s:\n', substring(opt$option, 6)), sep = ' ')
-        str(opt.value)
+        value = eval(parse(text = opt))
+        cat('\t', opt, ': ', value, '\n', sep = '')
       }
   }
-# show.options()
+## show.options()
 
-##____________________________________________________________________________||
+##__________________________________________________________________||
 if(length(dev.list()) == 0)  quartz(width = 4, height = 4)
 trellis.par.set(theme = arg.theme)
 if(arg.transparent) trellis.par.set(background = list(col = 'transparent'))
 # show.settings()
-                
 
-##____________________________________________________________________________||
+
+##__________________________________________________________________||
