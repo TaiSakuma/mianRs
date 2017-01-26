bf2d84574fe83c197063f1182b33e5fdac8d9f6b
Tai Sakuma <tai.sakuma@gmail.com>
2017-01-26 13:19:51 +0100

    copy custom_read_table.R from bdphi-scripts

---
+++ custom_read_table.R
@@ -0,0 +1,45 @@
+# Tai Sakuma <sakuma@cern.ch>
+
+##__________________________________________________________________||
+custom_read_table <- function(file)
+{
+  ## read_table() in readr 3.3.0 doesn't correctly determine the
+  ## widths of columns. In particular, this is because
+  ## whitespaceColumns(), used in fwf_empty(), doesn't correctly
+  ## determine the position of the beginnings of columns.
+
+  ## This function determines the widths of the columns from the first
+  ## line (header) of a table file and use read_fwf().
+  l <- read_lines(file, n_max = 1)
+
+  whitespace_pos <- gregexpr(' ', l)[[1]]
+  whitespace_pos <- c(0, whitespace_pos)
+  diff_pos <- c(0, diff(whitespace_pos))
+
+  begin <- c(0, whitespace_pos[diff_pos > 1])
+  end <- c(begin[2:length(begin)] - 1, str_length(l))
+
+  col_names <- str_split(l, ' ')[[1]]
+  col_names <- col_names[col_names != '']
+
+  col_positions <- list(
+    begin = begin,
+    end = end,
+    col_names = col_names
+  )
+
+  tbl <- read_fwf(file, col_positions = col_positions, col_types = cols(), skip = 1)
+  ## col_types = cols() suppresses the message "Parsed with column ...""
+  ## skip = 1 skips the header, the first line
+
+  ## convert 'character' to 'factor'
+  col_types <- sapply(tbl, typeof)
+  col_character <- names(col_types)[col_types == 'character']
+  for(col in col_character) {
+    tbl[[col]] <- factor(tbl[[col]])
+  }
+
+  tbl
+}
+
+##__________________________________________________________________||
